1f7abab8a659fa8f62e6c53c00749d2654691cab
bwdk-local <bowjeu@gmail.com>
2019-10-24 19:36:12 +0200

    adding ContactController, ContactRequest

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -2,4 +2,42 @@
 
 @section('content')
 <h1>Contact</h1>
+
+    @if ($errors->any())
+        <div class="alert alert-danger">
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
+
+<form method="post" action="{{ route('contact-form-submit') }}">
+    @csrf
+    <div class="form-group">
+            <label for="name">Name</label>
+            <input type="text" class="form-control" id="name" name="name" placeholder="Enter name" required>
+    </div>
+
+        <div class="form-group">
+          <label for="email">Email address</label>
+          <input type="email" class="form-control" id="email" name="email" aria-describedby="emailHelp" placeholder="Enter email">
+          <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+        </div>
+
+        <div class="form-group">
+                <label for="subject">Subject</label>
+                <input type="text" class="form-control" id="subject" name="subject" placeholder="Enter subject">
+        </div>
+
+        <div class="form-group">
+                <label for="message">Message</label>
+                <textarea class="form-control" id="message" name="message" rows="3"></textarea>
+        </div>
+      
+       
+        <button type="submit" class="btn btn-primary">Submit</button>
+      </form>
+
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -13,23 +13,26 @@
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('welcome');
 
 Route::get('/home', function () {
     return view('home');
-});
+})->name('home');
 
 Route::get('/page', function () {
     return view('page');
-});
+})->name('page');
 
 Route::get('/contact', function () {
     return view('contact');
-});
+})->name('contact');
 
 Route::get('/about', function () {
     return view('about');
-});
+})->name('about');
+
+Route::post('contact/submit', 'ContactController@submit')->name('contact-form-submit');
+
 
 Auth::routes();
 
